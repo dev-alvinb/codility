8b771989907acb3e88368eeeadb59dc9ac573c4e
dev-alvinb <136786195+dev-alvinb@users.noreply.github.com>
2023-09-08 12:19:14 +0800

    initial commit

---
+++ Codility/Codility/Data/ManagerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Codility.Models;
+
+namespace Codility.Data
+{
+    public class ManagerContext: DbContext
+    {
+        public ManagerContext(DbContextOptions<ManagerContext> options): base(options) { }
+
+        public DbSet<Applicant> Applicants { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
---
+++ Codility/Codility/Models/Applicant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Codility.Models
+{
+    public class Applicant
+    {
+        public Guid Id { get; set; } = new Guid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+    }
+}
